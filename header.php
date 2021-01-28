52bfc8dc8a6e6f6f16cee75d5350e253038a57a3
ajmaldeveloper <78089016+ajmaldeveloper@users.noreply.github.com>
2021-01-28 11:00:35 +0500

    product style

--- header.php
+++ header.php
@@ -46,7 +46,7 @@
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', '_s' ); ?></button>
+			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Menu', '_s' ); ?></button>
 			<?php
 			wp_nav_menu(
 				array(
