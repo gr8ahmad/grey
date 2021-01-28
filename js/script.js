331677b58687d37cea2f3c422335b63abc7d48e8
ajmaldeveloper <78089016+ajmaldeveloper@users.noreply.github.com>
2021-01-28 13:42:25 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 var cartIcon = document.createElement('span');
 cartIcon.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20.59 35.71"><defs><style>.cls-1{fill:#aaa297;}.cls-2{fill:none;stroke:#aaa297;stroke-miterlimit:10;}.cls-3{font-size:10.91px;fill:#efedea;font-family:MyriadPro-Regular, Myriad Pro;}</style></defs><g id="Layer_2" data-name="Layer 2"><g id="Layer_1-2" data-name="Layer 1"><rect class="cls-1" y="9.8" width="20.59" height="25.91"/><path class="cls-2" d="M6.31,11.13V4.49a4,4,0,0,1,4-4h0a4,4,0,0,1,4,4v6.64"/></g></g></svg>`;
-var masthead_li = document.querySelector('.cart-contents');
-masthead_li.appendChild(cartIcon);
+var cart_content = document.querySelector('.cart-contents');
+cart_content.appendChild(cartIcon);
   
